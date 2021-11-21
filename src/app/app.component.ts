bdc2857b131429a0ee01d079d6a0f08708e1047b
Jorge De Jesus Rivera Perez <i20163mc061@utez.edu.mx>
2021-11-21 11:43:35 -0600

    change to service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,12 +17,12 @@ export class AppComponent {
   likes: number = 0;
   posts: number = 0;
 
+
     constructor(private RestService: UserDataService){
 
     }
 
     ngOnInit(): void{
-
       // this.loadData();
 
 
@@ -47,7 +47,6 @@ export class AppComponent {
          fjs.parentNode.insertBefore(js, fjs);
        }(document, 'script', 'facebook-jssdk'));
 
-
     }
 
 
@@ -59,46 +58,29 @@ export class AppComponent {
           {
             if (response.authResponse)
             {
-
-              FB.api(
-                '/me',
-                'GET',
-                {"fields":"id,name,likes,posts,friends"},(responseA:any)=>{
-                  this.loadData(responseA)
-                }
-
-              );
-
-
-
+              this.mostrarDatos();
             }
              else
              {
              console.log('User login failed');
            }
-
         });
-
-
-
     }
 
-    public loadData(responseA: any){
+
+    public mostrarDatos(){
+      FB.api(
+        '/me',
+        'GET',
+        {"fields":"id,name,likes,posts,friends"},(responseA:any)=>{
           this.datosList = responseA;
           this.friends = this.datosList.friends.summary.total_count;
           this.likes = this.datosList.likes.data.length;
           this.posts = this.datosList.posts.data.length;
           console.log(this.posts);
+
+        }
+      );
     }
-    // public loadData(){
-    //   this.RestService.get('https://graph.facebook.com/v12.0/me?fields=id%2Cfriends%2Clikes%2Cposts&access_token=EAAJeXlNNvNEBAMu691J1VzK8HCwfdiEn9nWxOW3Y7rFS9xeg3lb9vlKMsmxDAlF5QgoFJV42KQEZCCoyD53coNCWUZAoeUEPLRlZCAQnmYkvM2Vo5Ia0AvRUIc8vqZAw9pEdkf34vZBqP8b9b2LJpZAADeZAwSUH1NBnnVEnZAd6Hx6KXwqpG7l1HZCQwnkqdlBOOIrtPzwr18UDulWpZA0UT6Cd1PolZC5cZBQPQQY80159F8V237wgryKu')
-    //   .subscribe(respuesta =>{
-    //     this.datosList = respuesta;
-    //     this.friends = this.datosList.friends.summary.total_count;
-    //     this.likes = this.datosList.likes.data.length;
-    //     this.posts = this.datosList.posts.data.length;
-    //     console.log(this.datosList.posts.data.length);
-    //   })
-    // }
 
 }
