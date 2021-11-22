d9c03732e7cc5feba44e21c1c2a80e1020cf2d4a
Jorge De Jesus Rivera Perez <i20163mc061@utez.edu.mx>
2021-11-21 23:16:00 -0600

    service works but not component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, EventEmitter } from '@angular/core';
 import { UserDataService } from './user-data.service';
 
 
@@ -21,18 +20,29 @@ export class AppComponent {
   loged: boolean = false;
 
 
+
   constructor(public _datosService: UserDataService){
 
+
+    _datosService.datosList1.subscribe(d => {
+      this.datosList = d;
+      console.log(this.datosList);
+      this.friends = this.datosList.friends.summary.total_count;
+      this.likes = this.datosList.likes.data.length;
+      this.posts = this.datosList.posts.data.length;
+      console.log(this.datosList);
+
+    });
 
   }
 
 conectarServicio(){
-
-  this.datosList = this._datosService.obtenerDatos();
-  this.friends = this.datosList.friends.summary.total_count;
-  this.likes = this.datosList.likes.data.length;
-  this.posts = this.datosList.posts.data.length;
-  console.log(this.datosList);
+  console.log("ahuevoooooo");
+  // this.datosList = this._datosService.obtenerDatos();
+  // this.friends = this.datosList.friends.summary.total_count;
+  // this.likes = this.datosList.likes.data.length;
+  // this.posts = this.datosList.posts.data.length;
+  // console.log(this.datosList);
 
   }
 
@@ -53,82 +63,15 @@ me() {
               this.friends = this.datosList.friends.summary.total_count;
               this.likes = this.datosList.likes.data.length;
               this.posts = this.datosList.posts.data.length;
-              console.log(this.posts);
+              console.log(this.friends);
 
             }
           );
 }
     ngOnInit(): void{
 
-
-      (window as any).fbAsyncInit = function() {
-
-        FB.init({
-          appId      : '666709170961617',
-          cookie     : true,
-          xfbml      : true,
-          version    : 'v12.0'
-        });
-        FB.AppEvents.logPageView();
-
-        FB.getLoginStatus((response: any) => {
-          if (response.status === 'connected') {
-
-            FB.api(
-              '/me',
-              'GET',
-              {"fields":"id,name,likes,posts,friends"},(responseA: any) => {
-                this.datosList = responseA;
-                this.friends = this.datosList.friends.summary.total_count;
-                this.likes = this.datosList.likes.data.length;
-                this.posts = this.datosList.posts.data.length;
-                console.log(this.posts);
-
-              }
-            );
-            // this.conectarServicio();
-            console.log('connected');
-
-        } else {
-
-          console.log('no connected');
-          this.login();
-
-        }
-
-      });
-      };
-
-      (function(d, s, id){
-        let js:any = null;
-        let fjs:any = null;
-        fjs = d.getElementsByTagName(s)[0];
-
-         if (d.getElementById(id)) {return;}
-         js = d.createElement(s) as HTMLScriptElement; js.id = id;
-         js.src = "https://connect.facebook.net/en_US/sdk.js";
-         fjs.parentNode.insertBefore(js, fjs);
-       }(document, 'script', 'facebook-jssdk'));
-
     }
 
 
-
-    //  public submitLogin(){
-
-    //   FB.api(
-    //     '/me',
-    //     'GET',
-    //     {"fields":"id,name,likes,posts,friends"},(responseA: any) => {
-    //       this.datosList = responseA;
-    //       this.friends = this.datosList.friends.summary.total_count;
-    //       this.likes = this.datosList.likes.data.length;
-    //       this.posts = this.datosList.posts.data.length;
-    //       console.log(this.posts);
-
-    //     }
-    //   );
-    //  }
-
-
 }
+
--- src/app/user-data.service.ts
+++ src/app/user-data.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 
 
@@ -14,25 +14,69 @@ export class UserDataService {
 
 
 
-  datosList: any = [];
 
-  public obtenerDatos(){
-            FB.api(
-              '/me',
-              'GET',
-              {"fields":"id,name,likes,posts,friends"},(responseA:any)=>{
-                this.datosList = responseA;
-                return this.datosList;
 
-              }
-            );
-  }
+  datosList1 = datosList;
 
   constructor() {
 
+    (window as any).fbAsyncInit = function() {
 
+      FB.init({
+        appId      : '666709170961617',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v12.0'
+      });
+      FB.AppEvents.logPageView();
+
+      FB.getLoginStatus((response: any) => {
+
+          if (response.status === 'connected'){
+            // setInterval(()=>{
+            FB.api(
+              '/me',
+              'GET',
+              {"fields":"id,name,likes,posts,friends"},(responseA: any):void =>{
+
+                    pasaDatos(responseA);
+
+              }
+            );
+
+          // },5000);
+          } else {
+
+            console.log('no connected');
+
+          }
+
+    });
+    };
+
+    (function(d, s, id){
+      let js:any = null;
+      let fjs:any = null;
+      fjs = d.getElementsByTagName(s)[0];
+
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s) as HTMLScriptElement; js.id = id;
+       js.src = "https://connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
 
   }
 
 
+
 }
+
+
+const datosList= new EventEmitter<number>();
+
+function pasaDatos (responseA: any){
+
+  datosList.emit(responseA);
+}
+
+
